ce0ee4efbad1d95c55f70ab969e4a4e8b7dff613
Alim Nagoev <nagoev.personal@gmail.com>
2023-09-11 12:56:24 +0300

    Add 05-site-ski-snowboard-school

---
+++ 05-site-ski-snowboard-school/README.md
@@ -0,0 +1,12 @@
+## Ski Snowboard School
+
+**Online snowboarding schools website.Learn think how pro with the help system online training on the snowboard which accelerates your progress**
+1. Made HTML-markup of all pages and all elements on the pages.
+2. One style file for all pages.
+3. Optimized the entire schedule.
+4. Implementation of the mobile menu and testimonials carousel using vanilla JavaScript.
+5. The document is being tested for validity [http://validator.w3.org](http://validator.w3.org/)
+6. Adaptive layout is implemented using media queries.
+7. Google Lighthouse: Performance - 100; Accessibility - 93;
+
+![Preview](images/preview.jpg)
---
+++ 05-site-ski-snowboard-school/scripts/main.js
@@ -0,0 +1,72 @@
+// LIBRARY
+// SCROLL TO SECTION ================================================
+(function (window) {
+	'use strict'; var document = window.document; var body = document.body; var rootElement = document.documentElement; var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || function (func) { window.setTimeout(func, 15) }; var clock = ''; var time = 500; var context = window; var start = context.scrollTop || window.pageYOffset; var end = 0; var easeInOutCubic = function (t) { return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1 }; var getTargetTop = function (target) {
+		var targetElement = {}; if (typeof target === 'number') { return target } else if (typeof target === 'string') {
+			targetElement = document.querySelector(target); if (!targetElement) { return !1 }
+			return targetElement.getBoundingClientRect().top + window.pageYOffset
+		}
+		return !1
+	}; var getScrollTop = function (startV, endV, elapsed, duration) {
+		if (elapsed > duration) { return endV }
+		return startV + (end - startV) * easeInOutCubic(elapsed / duration)
+	}; var getScrollPageBottom = function () { var contentHeight = Math.max.apply(null, [body.clientHeight, body.scrollHeight, rootElement.scrollHeight, rootElement.clientHeight]); return contentHeight - window.innerHeight }; var scrollFrame = function () {
+		var elapsed = Date.now() - clock; if (context === window) { window.scroll(0, getScrollTop(start, end, elapsed, time)) } else { context.scrollTop = getScrollTop(start, end, elapsed, time) }
+		if (elapsed <= time) { requestAnimationFrame(scrollFrame) }
+	}; var SmoothScroll = function SmoothScroll() { }; SmoothScroll.prototype = { scrollTo: function (target, duration, root) { clock = Date.now(); time = duration || 500; context = root || window; start = context.scrollTop || window.pageYOffset; end = getTargetTop(target); scrollFrame() }, scrollTop: function (duration, root) { this.scrollTo(0, duration, root) }, scrollBottom: function (duration, root) { this.scrollTo(getScrollPageBottom(), duration, root) } }; window.smoothScroll = new SmoothScroll()
+}(window))
+
+// MOBILE MENU ==================================
+const mobileMenu = (function () {
+	const hamburger = document.querySelector('.hamburger');
+	const menu = document.querySelector('.menu');
+	// events
+	hamburger.addEventListener("click", function () {
+		this.classList.toggle("is-active");
+		menu.classList.toggle('menu_open');
+	})
+})();
+mobileMenu;
+
+// SCROLL TO SECTION=========================================
+const scrollToSection = (function () {
+	const hamburger = document.querySelector('.hamburger');
+	const menu = document.querySelector('.menu');
+	var headerNavLinks = document.querySelectorAll('.menu__link');
+	// events
+	headerNavLinks.forEach(function (target) {
+		target.addEventListener('click', function (e) {
+			e.preventDefault();
+			smoothScroll.scrollTo(this.getAttribute('href'), 800);
+			menu.classList.remove('menu_open');
+			hamburger.classList.remove('is-active')
+		})
+	})
+
+})();
+scrollToSection;
+// SLIDER =========================================
+
+const slider = (function () {
+	// Variables declaration
+	const slider = document.querySelector('.slider__list');
+	let slideIndex = 0;
+
+	// functions
+	// Select index
+	function setIndex() {
+		document.querySelector('.slider__button_current').classList.remove('slider__button_current');
+		slider.style.transform = `translate(${slideIndex * -33.33}%)`;
+	}
+
+	// Event
+	// dots
+	document.querySelectorAll('.slider__button').forEach((dot, index) => {
+		dot.addEventListener('click', () => {
+			slideIndex = index;
+			setIndex();
+			dot.classList.add('slider__button_current');
+		});
+	});
+})();
+slider;
